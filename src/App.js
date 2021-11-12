e71d35768e4a6aeafa97a10099d89a25f0030b04
juank1809 <juank180999@gmail.com>
2021-11-12 10:03:20 -0500

    Deploying storybook

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Provider } from "react-redux";
+import store from "./lib/redux";
+
+import { Provider } from "react-redux";
+
+import InboxScreen from "./components/InboxScreen";
+
+import "./index.css";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <InboxScreen />
+    </Provider>
+  );
+}
+export default App;
